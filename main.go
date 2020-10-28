4f408691b13c8cd8bbdeb27e6b658b616ebe8ce7
Priyansh Agarwal <priyanshagarwal@rediff.com>
2020-10-28 20:44:46 +0530

    Adding code for Database and tables along with Query support for queries 1,2&8 and updating main

--- main.go
+++ main.go
@@ -7,12 +7,8 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
-	st := "SELECT COUNT(p) , q FROM t WHERE p = q AND p != 'q' GROUP BY q , p HAVING COUNT(p) > 1 AND AVG(q) < 3"
-=======
 	st := "SELECT COUNT(p) , q,r FROM table1 WHERE p = q AND p != 'q' GROUP BY q , p"
 	scanner := bufio.NewScanner(os.Stdin)
->>>>>>> Adding code for Database and tables along with Query support for queries 1,2&8
 	q, err := parser.Parse(st)
 	// if err == nil {
 	// 	fmt.Println(q)
